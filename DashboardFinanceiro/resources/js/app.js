2db6f4395761c07e6bef2867c7e60ce5c03e6851
Filipe Lamego <lamego@debian-lamego.debian-linux>
2024-10-19 23:48:23 -0300

    Nova versão do projeto, com docker no Linux

--- DashboardFinanceiro/resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'; // Importando createApp
-import App from './components/App.vue';  // Ajuste o caminho conforme necessário
-
-const app = createApp(App); // Criando a instância da aplicação
-app.mount('#app'); // Montando a aplicação no elemento com id "app"
